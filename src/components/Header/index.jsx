55a1539acf38eb69f51184d50d16927f66baac98
kiet.huynh <kiet.huynh@kyanon.digital>
2021-12-10 22:18:10 +0700

    feature: Init Src

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import "./style.scss";
+
+function index(props) {
+	return (
+		<header>
+			<div className="container">
+				<div className="inner"></div>
+			</div>
+		</header>
+	);
+}
+
+export default index;
